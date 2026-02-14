d0bf4d17a5b485c2fe3688a07277463724a85495
MihailJP <mihailjp@gmail.com>
2026-02-14 19:38:54 +0900

    Initial commit

---
+++ test/test_sample.py
@@ -0,0 +1,20 @@
+import pytest
+import fontforge
+
+
+@pytest.mark.parametrize(('data', 'glyphname'), [
+    (None, "A"),
+    (1, "B"),
+    (3, "C"),
+    (5, "D"),
+    (11, "E"),
+])
+def test_helloEnable(data, glyphname):
+    from fontforge_hello.__main__ import hello, helloEnable
+    try:
+        font = fontforge.font()
+        font.createMappedChar(glyphname)
+        assert helloEnable(data, font[glyphname]) == True
+        hello(data, font[glyphname])
+    finally:
+        font.close()
