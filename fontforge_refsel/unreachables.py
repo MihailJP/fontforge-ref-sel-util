30167177c22a84085bb783240028cabfacc069d1
MihailJP <mihailjp@gmail.com>
2026-03-02 03:05:19 +0900

    unusedGlyphs() now excludes such glyphs referred only from unreachable glyphs

--- fontforge_refsel/unreachables.py
+++ fontforge_refsel/unreachables.py
@@ -1,31 +1,36 @@
 import fontforge
 from numbers import Real
+from copy import deepcopy
 
 
 __all__ = ["unusedGlyphs", "selectUnusedGlyphs"]
 
 
-def _glyphIsNotEncoded(glyph: fontforge.glyph) -> bool:
-    return (glyph.unicode == -1) and (glyph.altuni is None)
+def _glyphIsEncodedOrDefault(glyph: fontforge.glyph) -> bool:
+    return (
+        (glyph.unicode >= 0) or
+        (glyph.altuni is not None) or
+        (glyph.glyphname == '.notdef')
+    )
 
 
-def _referredGlyphs(font: fontforge.font) -> frozenset[str]:
+def _referredGlyphs(font: fontforge.font, referFrom: set[str]) -> frozenset[str]:
     referred = set()
-    for glyph in font.glyphs():
-        for (ref, _, _) in glyph.references:
+    for glyph in referFrom:
+        for (ref, _, _) in font[glyph].references:
             referred.add(ref)
     return frozenset(referred)
 
 
-def _gsubGlyphs(font: fontforge.font) -> frozenset[str]:
+def _gsubGlyphs(font: fontforge.font, referFrom: set[str]) -> frozenset[str]:
     referred = set()
-    for glyph in font.glyphs():
-        for (_, lookupType, *lookupData) in glyph.getPosSub('*'):
+    for glyph in referFrom:
+        for (_, lookupType, *lookupData) in font[glyph].getPosSub('*'):
             if lookupType in ('Substitution', 'AltSubs', 'MultSubs', 'Ligature'):
                 for ref in lookupData:
                     referred.add(ref)
                 if lookupType == 'Ligature':
-                    referred.add(glyph.glyphname)
+                    referred.add(glyph)
     return frozenset(referred)
 
 
@@ -42,10 +47,15 @@ def unusedGlyphs(font: fontforge.font) -> frozenset[str]:
     :return: Names of unused glyphs
     :rtype: frozenset[str]
     """
-    unused = set([glyph.glyphname for glyph in filter(_glyphIsNotEncoded, font.glyphs())])
-    unused -= _referredGlyphs(font)
-    unused -= _gsubGlyphs(font)
-    unused -= {'.notdef', '.null'}
+    used = set([glyph.glyphname for glyph in filter(_glyphIsEncodedOrDefault, font.glyphs())])
+    delta = deepcopy(used)
+    while delta:
+        newDelta = set()
+        newDelta |= _referredGlyphs(font, delta)
+        newDelta |= _gsubGlyphs(font, delta)
+        used |= newDelta
+        delta = newDelta - used
+    unused = set(font) - used
     return frozenset(unused)
 
 
